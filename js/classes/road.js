7241cef62d7c6dd4bd7dc5a8b250e69308431988
Daniel Gil <danielgilucsd@gmail.com>
2020-03-09 10:02:08 -0700

    commit test

---
+++ js/classes/road.js
@@ -0,0 +1,12 @@
+class Road extends Phaser.GameObjects.Container
+{
+  constructor(config)
+  {
+    super(config.scene);
+    this.scene=config.scene;
+    this.back=this.scene.add.image(0,0,"road");
+    this.add(this.back);
+    this.scene.add.existing(this);
+  }
+
+}
--- js/sceneMain.js
+++ js/sceneMain.js
@@ -4,17 +4,23 @@ class SceneMain extends Phaser.Scene {
     }
     preload()
     {
-    	
+        //this is were we load images our sounds
+        this.load.image("road","images/road.jpg");
+        this.load.spritesheet("cars","images/cars.png",{frameWidth:60,frameHeight:126})
     }
     create() {
-        console.log("Ready!");
+        //define our objects
+        var road=new Road({scene:this});
     }
-    update() {}
-}
+    update() 
+    {
+        
+    }
+
+ }
 
 
 
 
 
-
-
+///comment
